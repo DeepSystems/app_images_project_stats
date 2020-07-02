e4ae15ae66790980cc9c0b5c3937aacd191e8261
Max Kolomeychenko <max@supervise.ly>
2020-07-02 14:51:14 +0300

    simple progress and table done, need web modification

--- src/main.py
+++ src/main.py
@@ -7,22 +7,56 @@ my_app = sly.AppService()
 
 BG_COLOR = [0, 0, 0]
 
+
+def area_name(name):
+    return "{} [area %]".format(name)
+
+
+def count_name(name):
+    return "{} [count]".format(name)
+
+
+def color_name(name, color):
+    return '<b style="display: inline-block; border-radius: 50%; background: {}; width: 8px; height: 8px"></b> {}'.format(color, name)
+
+
+def color_tag_name(name, color):
+    return '<i class="zmdi zmdi-label" style="color:{};margin-right:3px"></i>{}'.format(sly.color.rgb2hex(color), name)
+
+
+def rename_fields(data, old_names, new_name_func, colors=None, color_func=None):
+    colors = sly.take_with_default(colors, [None] * len(old_names))
+    for name, color in zip(old_names, colors):
+        if name in data:
+            new_name = new_name_func(name)
+            if color is not None and color_func is not None:
+                new_name = color_func(new_name, color)
+            data[new_name] = data[name]
+            del data[name]
+
+
 @my_app.callback("calculate")
 @sly.timeit
 def calculate(api: sly.Api, task_id, context, state):
-    project_id = 11#context.get("project_id")
+    project_id = 502 #context.get("project_id")
     project = api.project.get_info_by_id(project_id)
     if project is None:
         raise RuntimeError("Project ID={!r} not found".format(project_id))
+    if project.type != str(sly.ProjectType.IMAGES):
+        raise RuntimeError('Project {!r} has type {!r}. This script works only with {!r} projects'
+                           .format(project.name, project.type, str(sly.ProjectType.IMAGES)))
 
-    #@TODO: uncomment
-    #if project_id is None:
-    #    sly.logger.critical("Project ID nor found")
+    workspace = api.workspace.get_info_by_id(project.workspace_id)
+    team = api.team.get_info_by_id(workspace.team_id)
+
+    sly.logger.info("team: {}".format(team.name))
+    sly.logger.info("workspace: {}".format(workspace.name))
+    sly.logger.info("project: {}".format(project.name))
 
     meta_json = api.project.get_meta(project_id)
     meta = sly.ProjectMeta.from_json(meta_json)
 
-    # list classes
+    # list classes (used when several classes have the same colors )
     class_names = []
     class_colors = []
     class_indices = []  # 0 - for unlabeled area
@@ -43,24 +77,22 @@ def calculate(api: sly.Api, task_id, context, state):
         tag_names.append(tag_meta.name)
         tag_colors.append(tag_meta.color)
 
-    # check colors uniq
-    color_names = defaultdict(list)
-    for name, color in zip(class_names, class_colors):
-        hex = sly.color.rgb2hex(color)
-        color_names[hex].append(name)
+    total_images_count = api.project.get_images_count(project.id)
 
-    class_colors_notify = ""
-    for k, v in color_names.items():
-        if len(v) > 1:
-            warn_str = "Classes {!r} have the same RGB color = {!r}".format(v, sly.color.hex2rgb(k))
-            sly.logger.warn(warn_str)
-            class_colors_notify += warn_str + '\n\n'
+    #total_images_in_project = 0
+    #stats_area = []
+    #stats_count = []
+    #stats_img_tags = []
 
+    table_per_image_stats = []
+    table_batch = []
     for dataset in api.dataset.get_list(project.id):
         images = api.image.get_list(dataset.id)
+        ds_progress = sly.Progress('Dataset {}'.format(dataset.name), total_cnt=len(images))
+
         for batch in sly.batched(images):
             image_ids = [image_info.id for image_info in batch]
-            image_names = [image_info.name for image_info in batch]
+            #image_names = [image_info.name for image_info in batch]
 
             ann_infos = api.annotation.download_batch(dataset.id, image_ids)
             ann_jsons = [ann_info.annotation for ann_info in ann_infos]
@@ -71,45 +103,40 @@ def calculate(api: sly.Api, task_id, context, state):
                 render_img = np.zeros(ann.img_size + (3,), dtype=np.uint8)
                 render_img[:] = BG_COLOR
                 ann.draw(render_img)
-                # temp_area = sly.Annotation.stat_area(render_img, class_names, class_colors)
 
                 render_idx_rgb = np.zeros(ann.img_size + (3,), dtype=np.uint8)
                 render_idx_rgb[:] = BG_COLOR
                 ann.draw_class_idx_rgb(render_idx_rgb, _name_to_index)
-                temp_area = sly.Annotation.stat_area(render_idx_rgb, class_names, class_indices_colors)
+
+                temp_area = sly.Annotation.stat_area(render_idx_rgb, class_names, class_indices_colors, percent=True)
+                rename_fields(temp_area, class_names, area_name, class_colors, color_name)
 
                 temp_count = ann.stat_class_count(class_names)
+                rename_fields(temp_count, class_names, count_name, class_colors, color_name)
+
+                temp_img_tags = {}
                 if len(tag_names) != 0:
                     temp_img_tags = ann.stat_img_tags(tag_names)
+                    rename_fields(temp_img_tags, tag_names, lambda x: x, tag_colors, color_tag_name)
 
                 temp_area['id'] = info.id
                 temp_area['dataset'] = dataset.name
                 temp_area['name'] = '<a href="{0}" rel="noopener noreferrer" target="_blank">{1}</a>' \
-                    .format(api.image.url(team.id,
-                                          workspace.id,
-                                          project.id,
-                                          dataset.id,
-                                          info.id),
-                            info.name)
+                    .format(api.image.url(team.id, workspace.id, project.id, dataset.id, info.id), info.name)
 
-                stats_area.append(temp_area)
-                stats_count.append(temp_count)
-                if len(tag_names) != 0:
-                    stats_img_tags.append(temp_img_tags)
+                table_batch.append({**temp_area, **temp_count, **temp_img_tags})
 
             ds_progress.iters_done_report(len(batch))
-            total_images_in_project += len(batch)
-    #@TODO: implement here
-    #if class_colors_notify != "":
-    #    widgets.append(
-    #        api.report.create_notification("Classes colors", class_colors_notify, sly.NotificationType.WARNING))
+            table_per_image_stats.extend(table_batch)
 
-    print("Hello!")
-    # new_str = sly.rand_str(10)
-    # api.app.set_vars(task_id, "data.randomString", new_str)
+            # refresh table and progress
+            payload = {
+                "progress": int(len(table_per_image_stats) / total_images_count * 100),
+                "tablePerImageStats": table_batch
+            }
+            api.app.set_data(task_id, payload, "data", append=True)
 
 
-#@TODO: show warning
 def main():
     table = []
     for i in range(10):
@@ -117,13 +144,15 @@ def main():
 
     # data
     data = {
-        "table": table
+        "tablePerImageStats": table,
+        "progress": 0
     }
 
     # state
     state = {
         "perPage": 25,
-        "pageSizes": [25, 50, 100]
+        "pageSizes": [25, 50, 100],
+        "processingFlag": True
     }
 
     # start event after successful service run
